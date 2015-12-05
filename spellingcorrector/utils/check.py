19ed35c4e440a484a97a9b155220d8757b99fa29
ting.wang <ting.wang@easystack.cn>
2015-12-05 15:52:23 +0800

    50%

--- spellingcorrector/utils/check.py
+++ spellingcorrector/utils/check.py
@@ -1,3 +1,64 @@
+import functools
+
+from spellingcorrector.utils import count
+
+_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 def check():
     pass
+
+
+def checkIns():
+
+    def wrapper(fn):
+        @functools.wraps(fn)
+        def new_func(*args, **kwargs):
+            ins = args[1]
+            if isinstance(ins, count.Count):
+                args = list(args)
+                return fn(*args, **kwargs)
+            else:
+                raise TypeError(
+                    'must passed into an instance of utils.count:Conut')
+        return new_func
+    return wrapper
+
+
+def edit1(words):
+    """
+    edit distance = 1
+    """
+    split = [(words[:i], words[1:]) for i in len(words)]
+    deletion = [a + b[1:] for a,b in split if b]
+    insertion = [a + c + b for a,b in split for c in _ALPHABET]
+    alteration = [a + c + b[1:] for a,b in split for c in _ALPHABET]
+    transposition = [a + b[1] + b[0] + b[2:] for a,b in split if len(b)>1]
+    return set(deletion + insertion + alteration + transposition)
+
+
+
+@checkIns
+def edit2(words, ins):
+    """
+    edit diatance = 2
+    """
+    return set(e2 for e1 in edit1(words)
+               for e2 in edit1(e1)
+               if e2 in ins.nwords)
+
+
+@checkIns
+def edit0(words, ins):
+    """
+    edit distance = 0, that's say the word is correct.
+    """
+    return set(e0 for e0 in words
+               if e0 in ins.nwords)
+
+
+@checkIns
+def correct(words, ins):
+    candidates = edit0(words, ins) or edit1(words) \
+                    or edit2(words, ins) or [words]
+    return max(candidates).keys()[0]
+
--- spellingcorrector/utils/count.py
+++ spellingcorrector/utils/count.py
@@ -1,21 +1,42 @@
+import os
+import sys
+import traceback
+import functools
 
-class Count(object):
+    _CORPUS_DIR = '../data/corpus.txt'
+
+def checkCorpus(fn):
+    @functools.wraps(fn)
+    def new_func(ins, *args, **kwargs):
+        if not os.path.isfile(Count._CORPUS_DIR):
+            raise IOError('cannot find corpus in data/')
+        return fn(ins, *args, **kwargs)
+    return  new_func
+
+
+
+
 
     def __init__(self):
         self.corpus = []
+        self.nword = []
+        try:
+            self.train()
+        except IOError:
+            print traceback.print_exc(file=sys.stderr)
 
 
-    def checkCorpus(self):
-        pass
 
-    def addCorpus(self, dir):
-        isValid(dir)
+    @checkCorpus
+    def train():
+        f = file(_CORPUS_DIR).open()
+        for line in f:
+            split = line.split()
+            tmp = {split[0]:split[1]}
+            nword.append(tmp)
 
-    def train(self):
-        pass
+if __name__ == "__main__":
+    c = Count()
 
-def isValid(dir):
-    """
-    check if a valid file dir
-    """
-    pass
+
+
